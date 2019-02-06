e01d3defc006cee59cd123bfe6c2102f0d5944a3
asabagh <a.sabagh72@gmail.com>
2019-02-06 17:22:29 +0330

    combine search and single include of page and post and archive include category and tags

---
+++ templates/header.php
@@ -0,0 +1,21 @@
+<?php
+$current_object = get_queried_object();
+if (is_search()) {
+    ?>
+    <header class="single-header">
+        <h1 class="single-title text-center">نمایش جستجو بر اساس  <span class="search-item"><?php echo $s; ?></span></h1>
+    </header>
+    <?php
+} elseif (is_single()) {
+    ?>
+    <header class="single-header">
+        <h1 class="single-title text-center"><?php the_title(); ?></h1>
+    </header>
+    <?php
+} else {
+    ?>
+    <header class="single-header">
+        <h1 class="single-title text-center"><?php echo $current_object->name; ?></h1>
+    </header>
+    <?php
+}
