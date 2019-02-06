d137ce1e8439208ccc0da9d35546c19ccc8a6885
asabagh <a.sabagh72@gmail.com>
2019-02-06 17:14:36 +0330

    loop with content

---
+++ templates/archive/loop-content.php
@@ -0,0 +1,14 @@
+<?php
+$post_id = get_the_ID();
+$like_count = (get_post_meta($post_id, "lj_like_wp", TRUE)) ? get_post_meta($post_id, "lj_like_wp", TRUE) : 0;
+?>
+<article class="d-flex flex-wrap  post-item align-items-top">
+    <div class="flex-grow-1 post-title" >
+        <i class="fa fa-file-text-o"></i>
+        <a href="" ><?php the_title(); ?></a>
+    </div>
+    <div class="post-meta d-flex flex-column text-md-left">
+        <span class="date"><?php the_date(); ?></span>
+        <span class="like"><?php echo $like_count; ?></span>
+    </div>
+</article>
